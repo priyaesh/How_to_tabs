43ade9e827e1f618bd6dedab3458da84893ef6b5
unknown <hemapriyasampath@gmail.com>
2019-02-12 14:58:00 -0500

    assertEqual

--- src/test.js
+++ src/test.js
@@ -1,3 +1,14 @@
 (function(){
     "use strict";
+
+    assertEqual(add(3,4), 7);
+    
+
+    function add (a, b){
+        return a+b;
+    }
+
+    function assertEqual(actual, expected){
+        if(actual!==expected) throw new Error("Expected "+expected +",but got "+actual);
+    }
 }());
