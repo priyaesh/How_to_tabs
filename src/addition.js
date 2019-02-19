34766bf5f461f9238a283738bee3b7409c2d36d1
unknown <hemapriyasampath@gmail.com>
2019-02-19 16:01:18 -0500

    generated and dist

--- src/addition.js
+++ src/addition.js
@@ -1,23 +1,8 @@
 (function(){
     "use strict";
 
-    var addition = require("./_addition_test");
-   
-
-    describe("Addition",function(){
-        it("add positive numberes", function(){
-            assertEqual(addition.add(3,4),7);
-        });
-        it("uses IEEE 754 floating point",function(){
-            assertEqual(addition.add(0.1,0.2),0.30000000000000004);
-        });
-        function assertEqual(actual, expected){
-            if(actual!==expected) throw new Error("expected"+expected+",but was "+actual);
-        }
-    });
-
-    function add (a, b){
+   exports.add= function add (a, b){
         return a+b;
-    }
+    };
 
 }());
