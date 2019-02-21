4995c05bcb699aa72f75b5747898ceee4d60f002
unknown <hemapriyasampath@gmail.com>
2019-02-21 11:27:11 -0500

    adding third party module --an isolation

--- src/javascript/_addtion_test.js
+++ src/javascript/_addtion_test.js
@@ -2,7 +2,7 @@
     "use strict";
 
     var addition = require("./addition.js");
-    var assert = require("../vendor/chai-2.1.js").assert;
+    var assert = require("./assert.js");
 
     describe("Addition",function(){
         it("add positive numberes", function(){
---
+++ src/javascript/assert.js
@@ -0,0 +1,8 @@
+(function(){
+    "use strict";
+
+    var assert = require("../vendor/chai-2.1.js").assert;
+
+    module.exports = assert;
+
+}());
