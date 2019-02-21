c385bde2969488c04da76f04e097586b3014e3cc
unknown <hemapriyasampath@gmail.com>
2019-02-21 14:27:23 -0500

    setup a bare bone

--- src/javascript/_addtion_test.js
+++ src/javascript/_addtion_test.js
@@ -1,21 +1,14 @@
 (function(){
     "use strict";
 
-    var addition = require("./addition.js");
+    
     var assert = require("./assert.js");
 
-    describe("Addition",function(){
-        it("add positive numberes", function(){
-            assert.equal(addition.add(3,4),7);
+    describe("something",function(){
+        it("something", function(){
         });
-        it("uses IEEE 754 floating point",function(){
-            assert.equal(addition.add(0.1,0.2),0.30000000000000004);
-        });
-        
+       
     });
 
-    function add (a, b){
-        return a+b;
-    }
-
+   
 }());
