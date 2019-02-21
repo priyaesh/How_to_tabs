f4388387c6a2395845c8fa9db3c76969830de6fe
unknown <hemapriyasampath@gmail.com>
2019-02-21 11:17:04 -0500

    installed chai for assertion

--- src/javascript/_addtion_test.js
+++ src/javascript/_addtion_test.js
@@ -6,14 +6,12 @@
 
     describe("Addition",function(){
         it("add positive numberes", function(){
-            assertEqual(addition.add(3,4),7);
+            assert.equal(addition.add(3,4),7);
         });
         it("uses IEEE 754 floating point",function(){
-            assertEqual(addition.add(0.1,0.2),0.30000000000000004);
+            assert.equal(addition.add(0.1,0.2),0.30000000000000004);
         });
-        function assertEqual(actual, expected){
-            if(actual!==expected) throw new Error("expected"+expected+",but was "+actual);
-        }
+        
     });
 
     function add (a, b){
